2410893001d3045f05f598075f61960bb4bfba8c
sere06001 <sere06001@utb.vaxjo.se>
2024-02-15 14:15:54 +0100

    Add project files.

---
+++ Uppgift 6.5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace uppgift6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två strängar");
+            Console.Write("Sträng 1: ");
+            string a = Console.ReadLine();
+            Console.Write("Sträng 2: ");
+            string b = Console.ReadLine();
+            Längst(a, b);
+        }
+        static void Längst(string a, string b)
+        {
+            if (a.Length > b.Length)
+            {
+                Console.WriteLine($"Sträng 1 är den längsta strängen");
+            }
+            else if (b.Length > a.Length)
+            {
+                Console.WriteLine($"Sträng 2 är den längsta strängen");
+            }
+            else
+            {
+                Console.WriteLine("Båda strängarna är lika långa");
+            }
+        }
+    }
+}
